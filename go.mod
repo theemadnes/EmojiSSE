09b807f228bf4bf64373d3160699146ac63f76f1
theemadnes <alex.mattson@gmail.com>
2022-11-10 21:52:18 -0600

    sort of working for random emoji return in func

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module theemadnes/EmojiStream
 go 1.19
 
 require (
+	github.com/enescakir/emoji v1.0.0 // indirect
+	golang.org/x/exp v0.0.0-20221110155412-d0897a79cd37 // indirect
 	golang.org/x/net v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/enescakir/emoji"
+	"golang.org/x/exp/maps"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
@@ -17,12 +19,38 @@ func getEnv(key, fallback string) string {
 	return fallback
 }
 
+// get a random Emoji
+func getRandomEmoji() string {
+	// set random seed
+	//rand.Seed(time.Now().UnixNano())
+	emojiMap := emoji.Map()
+	var emojiValues []string = maps.Values(emojiMap)
+	//emojiValues[1]
+	//randomEmoji := emojiValues[0]
+	return emojiValues[0]
+}
+
 func main() {
+	H2CServerUpgrade()
+	//H2CServerPrior()
+}
+
+func checkErr(err error, msg string) {
+	if err == nil {
+		return
+	}
+	fmt.Printf("ERROR: %s: %s\n", msg, err)
+	os.Exit(1)
+}
+
+func H2CServerUpgrade() {
 	port := getEnv("PORT", "8080")
 	h2s := &http2.Server{}
 
 	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %v, http: %v", r.URL.Path, r.TLS == nil)
+		fmt.Printf("Got request\n")
+		//fmt.Fprintf(w, "Hello, %v, http: %v", r.URL.Path, r.TLS == nil)
+		fmt.Fprintf(w, "%v", getRandomEmoji())
 	})
 
 	server := &http.Server{
@@ -32,8 +60,4 @@ func main() {
 
 	fmt.Printf("Listening [0.0.0.0:%s]...\n", port)
 	checkErr(server.ListenAndServe(), "while listening")
-}
-
-func checkErr(err error, s string) {
-	panic("unimplemented")
 }
